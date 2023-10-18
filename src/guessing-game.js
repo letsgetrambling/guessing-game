8df7f8ac74922b462e5feae4b8f554094e4c4aa7
Konstantsin <konstantinlemesh@yandex.by>
2023-10-18 23:28:27 +0300

    feat: complete guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
+      
     }
 
     guess() {
-
+        this.middle = Math.floor((this.min + 1 + this.max) / 2);
+        return this.middle;
     }
-
+      
     lower() {
+        this.max = this.middle;
 
     }
 
     greater() {
-
+        this.min = this.middle;
     }
+    
 }
 
 module.exports = GuessingGame;
